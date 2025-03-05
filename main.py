1762b0ef27698e95a3fceaae27c05cb200e10884
wwwking837 <muhammadzaroonadnan@gmail.com>
2025-03-05 20:25:29 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import datetime
+
+# using now() to get current time
+current_time = datetime.datetime.now()
+
+# printing value of now
+print("Time now at greenwich meridian is:", end = "")
+print(current_time)
+
+# Print calender of year 2021
+import calendar
+print("\n", calendar.calendar(2025))
